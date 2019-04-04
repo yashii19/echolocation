19fb1942517e2c3aa8b531d1757c502d2d8273ee
Alexandre Janin <alexandrejanin92@gmail.com>
2019-04-04 17:52:04 +0200

    Ajout buzzer/potentiometre

---
+++ buzzer_potentiometre/buzzer_potentiometre.ino
@@ -0,0 +1,22 @@
+#define potPin A0
+#define buzzPin 8
+
+#define potMin 0
+#define potMax 1023
+#define freqMin 50
+#define freqMax 2000
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buzzPin, OUTPUT);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  Serial.println(potValue);
+
+  int pitch = map(potValue, potMin, potMax, freqMin, freqMax);
+
+  tone(buzzPin, pitch, 50);
+  //delay(50);
+}
